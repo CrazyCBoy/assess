9718dab0d9c7bf7c85de3686b860128ca9717522
CrazyCBoy <370551444@qq.com>
2019-04-29 10:05:03 +0800

    one

---
+++ assess-prec/frontend/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p> Header!!!!   北京云网高科 2019</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+  }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
